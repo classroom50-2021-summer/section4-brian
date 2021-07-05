e609a0b3851068bead848dbb08037f0156e53602
Brian Yu <brianyu28@gmail.com>
2021-07-05 10:51:29 -0400

    Section 4

---
+++ pdf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    // Check for proper usage
+    if (argc != 2)
+    {
+        printf("Usage: ./pdf filename\n");
+        return 1;
+    }
+
+    // TODO: check if file is a PDF
+}
